6ae9070fc57e2ba7e7a5647e18c860f0c1732217
mgluf <gluf.matt@gmail.com>
2021-09-27 18:30:50 -0400

    pulling all streams in NC data into JSON

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,26 @@
 <script>
-	export let name;
+  let myBody
+	const userAction = async () => {
+  const response = await fetch('https://waterservices.usgs.gov/nwis/iv/?format=json&stateCd=nc&parameterCd=00060,00065&siteType=ST&siteStatus=all', {
+    method: 'POST',
+    body: myBody, // string or object
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  const myJson = await response.json(); //extract JSON from the http response
+  // do something with myJson
+  console.log(myJson);
+}
+
+userAction()
+
+
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>Hello</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
