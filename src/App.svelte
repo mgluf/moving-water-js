161d17955777ed23c01915f50e15454187f9c5ba
mgluf <gluf.matt@gmail.com>
2022-02-23 21:04:07 -0500

    readings attained

--- src/App.svelte
+++ src/App.svelte
@@ -4,22 +4,24 @@ import parseStations  from "./parseStations.js";
 import fetchReadings from "./fetchReadings.js";
 
 let stations
-let readings =[]
+let readings = [];
 
 // Pull 20 Stations with a waterFlow measurement
 onMount(
 	async () => {
 		fetch("https://environment.data.gov.uk/hydrology/id/stations?_limit=20&observedProperty=waterFlow")
 		.then(response => response.json())
-		.then(data => {
+		.then(async (data) => {
 			console.log("raw", data);
 			stations = parseStations(data)
 			console.log("stations", stations);
 
 			// pull readings from each station from 4 days ago to today
-			readings = stations.map(station => {
-				fetchReadings(station.notation, 4)
-			})
+      for (const station of stations) {
+        readings.push(await fetchReadings(station.notation, 4))				
+      }
+
+      console.log("readings", readings)
 
 		}).catch(error => {
 			console.log(error);
--- src/fetchReadings.js
+++ src/fetchReadings.js
@@ -1,6 +1,6 @@
 import parseReadings from "./parseReadings";
 
-export default function fetchReadings(notation, days){
+export default async function fetchReadings(notation, days){
 
 let today = new Date();
 // define the number of days before today to look for readings
@@ -14,27 +14,28 @@ formatted =  year + '-' + ('0' + month).slice(-2) + '-' + ('0' + day).slice(-2);
 
 
 
-
-
 // make API call with provided notation and days back to look for readings
 let fetched
+// let post
 const prefix = "https://environment.data.gov.uk/hydrology"
 
-fetch(prefix + "/id/measures/" + notation + "-flow-m-86400-m3s-qualified/readings?mineq-date="+ formatted)
+return await fetch(prefix + "/id/measures/" + notation + "-flow-m-86400-m3s-qualified/readings?mineq-date="+ formatted)
 		.then(response => response.json())
 		.then(data => {
 			
-			// console.log("fetchReadings RAW", "\n" , data);
-			console.log("parseReadings", parseReadings(data));
 			fetched = parseReadings(data);
-			
+      // console.log("fetched", fetched)
+			return fetched;
 
 		}).catch(error => {
 			console.log(error);
 			return [];
 		});
 
-console.log("fetchReadings", fetched)
 
-return fetched
+// console.log(post);
+
+// return fetched;
 }
+
+
