ca51fb0462fa01c5699454142de265183609066f
Matt <gluf.matt@gmail.com>
2022-02-24 18:05:05 -0500

    infobar component

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,24 @@
 <script>
 import { onMount } from "svelte";
-import parseStations  from "./parseStations.js";
-import fetchReadings from "./fetchReadings.js";
+import parseStations  from "./data-functions/parseStations.js";
+import fetchReadings from "./data-functions/fetchReadings.js";
+
+import InfoBar from "./components/InfoBar.svelte"
 
 let stations
 
 // Pull 20 Stations with a waterFlow measurement
 onMount(
-	async () => {
+	async () =>  {
 		fetch("https://environment.data.gov.uk/hydrology/id/stations?_limit=20&observedProperty=waterFlow")
 		.then(response => response.json())
 		.then(async (data) => {
 			console.log("raw", data);
 			stations = parseStations(data)
-
-			// pull readings from each station from 4 days ago to today
-      for (const station of stations) {
-				station.readings = await fetchReadings(station.notation, 4);				
-      }
-			console.log("stations with readings", "\n", stations);
-
+			// pull readings from each station from 4 days ago to today and add readings to them
+			for (const station of stations) {station.readings = await fetchReadings(station.notation, 4);}
+			console.log("onMount", stations)
+	
 		}).catch(error => {
 			console.log(error);
 			return [];
@@ -28,11 +27,9 @@ onMount(
 
 </script>
 
-<main>
-	<h1>River Water</h1>
-  <div>
-  </div>
-</main>
+{#if stations}
+		<InfoBar stations={stations}/>
+{/if}
 
 <style>
 
---
+++ src/components/InfoBar.svelte
@@ -0,0 +1,32 @@
+<script>
+
+  export let stations;
+  console.log("infoBar", stations)
+  // console.log(stations[0].readings.value)
+
+
+</script>
+
+<nav class="info-bar">
+  {#each stations as station}
+  <ul>
+    <li>{station.river}</li>
+    <li>{station.label}</li>
+    <li>
+      <!-- {station.readings.value} -->
+      {station.unit}
+    </li>
+    <li>{station.measure}</li>
+    <!-- <li>Data recored on {station.readings.date}</li> -->
+  </ul>
+  {/each}
+</nav>
+
+
+
+
+
+
+<style>
+
+</style>
